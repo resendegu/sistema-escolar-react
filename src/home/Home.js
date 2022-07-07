9900602e7ec2c093194f2c91e21e2f8131717d9f
Gustavo Resende <gustavo@resende.app>
2022-07-07 07:45:50 -0300

    Update Home.js

--- src/home/Home.js
+++ src/home/Home.js
@@ -2,6 +2,7 @@ const Home = () => {
     return (
         <div>
             <h1>Seja bem vindo ao Sistema Escolar!</h1>
+            <p>{window.location.hostname === 'escola.resende.app' && 'Esté é uma ambiente de desenvolvimento, e pode ser instável. Não recomendamos inserir informações sensíveis neste ambiente.'}</p>
         </div>
     );
 }
